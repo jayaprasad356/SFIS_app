e2ff87fabdc1c0d2a4c101a696f4bb7fc89f9f8b
chhagan <chhaganswami1@gmail.com>
2023-01-30 15:42:05 +0530

    newone

---
+++ app/src/main/java/com/graymatter/sfis/MyDairy.kt
@@ -0,0 +1,11 @@
+package com.graymatter.sfis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyDairy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_dairy)
+    }
+}
