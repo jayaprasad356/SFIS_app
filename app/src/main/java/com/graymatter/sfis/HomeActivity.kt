42d1564c467b439208df7f9f153cb11197c134ba
bharadwaj <ravinuthalabharadwaj2112@gmail.com>
2023-02-15 11:55:43 +0530

    added bottomsheet fot rcSearch

--- app/src/main/java/com/graymatter/sfis/HomeActivity.kt
+++ app/src/main/java/com/graymatter/sfis/HomeActivity.kt
@@ -3,15 +3,37 @@ package com.graymatter.sfis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_rc_search.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheetDialog : BottomSheetDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         rcdetail.setOnClickListener { startActivity(Intent(this,RCDetail::class.java)) }
       profilebtn.setOnClickListener { startActivity(Intent(this,ProfileActivity::class.java)) }
         share.setOnClickListener { startActivity(Intent(this,ReferralActivity::class.java)) }
+        RcSearch.setOnClickListener {
+            bottomSheetDialog.show()
+        }
+
+        bottomSheetDialog = BottomSheetDialog(this@HomeActivity).also {
+            it.setContentView(R.layout.rc_search_bottom_sheet)
+
+            val edBikeNumber = it.findViewById(R.id.edBikeNumber) as EditText?
+            val btnSearch = it.findViewById(R.id.btnSearch) as Button?
+
+            btnSearch?.setOnClickListener {
+                Intent(this,RcSearchActivity::class.java).also { intent->
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
